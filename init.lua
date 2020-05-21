b44ec4d5cee511e9230197f772c0d588127bc3a3
pithydon <daniel@Daniels-iMac.local>
2020-05-20 22:10:15 -0600

    Import

---
+++ init.lua
@@ -0,0 +1,39 @@
+minetest.register_tool("random_place_tool:random_place_tool", {
+	description = "Random Place Tool",
+	inventory_image = "random_place_tool.png",
+	wield_image = "random_place_tool.png",
+	on_place = function(itemstack, placer, pointed_thing)
+		local inv = placer:get_inventory()
+		local wield_index = placer:get_wield_index()
+		local items = {}
+		for i=1,8 do
+			if wield_index ~= i then
+				local itemstack = inv:get_stack("main", i)
+				local name = itemstack:get_name()
+				local item = minetest.registered_items[name]
+				if not itemstack:is_empty() and item then
+					table.insert(items, {itemstack, item.on_place, i})
+				end
+			end
+		end
+		if items[1] then
+			local item = items[math.random(#items)]
+			local func = item[2]
+			local var = func(item[1], placer, pointed_thing)
+			if type(var) == "userdata" and var:is_known() then
+				inv:set_stack("main", item[3], var)
+			end
+		end
+	end
+})
+
+if minetest.get_modpath("default") then
+	minetest.register_craft({
+		output = "random_place_tool:random_place_tool",
+		recipe = {
+			{"", "", "default:mese_crystal"},
+			{"", "group:stick", "default:mese_crystal"},
+			{"group:stick", "", ""}
+		}
+	})
+end
